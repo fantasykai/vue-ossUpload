5cc249fa565688577815600ce9052f497e465e6f
fantasykai <makren@126.com>
2017-06-17 18:10:51 +0800

    构建上传Ali-OSS上传组件

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@
       }
     },
     methods: {
-      //初始化OSS 权限
+      //初始化OSS 权限, 建议后台提供获取oss临时权限的接口
       initOSSAuth(){
 
         let ossSts = JSON.parse(localStorage.getItem('ossSts'));
@@ -45,12 +45,12 @@
         let Oss = OSS.Wrapper;
 
         this.ossClient = new Oss({
-          region: 'oss-cn-beijing',
-          accessKeyId: 'STS.KiWhDb3s9JQfQTXLFosT3bK6Y',
-          accessKeySecret: 'YhdRHdJrS56vJvfuvPatmaGzQGWS9wWhFHch3pqBiM1',
-          stsToken: 'CAIS/wF1q6Ft5B2yfSjIqqriI/7W3qwY/ZONU3LpqEY6f9sfjY6duzz2IHlNfnJtAO4XtPsyn2lS7fcTlr90UIQAXV3Ybcx2q49b8kasc4PEo8i4tUeBAFkGWjr9MQXy+eOPScebJYqvV5XAQlTAkTAJstmeXD6+XlujHISUgJp8FLo+VRW5ajw0b7U/ZHEVyqkgOGDWKOymPzPzn2PUFzAIgAdnjn5l4qnNqa/1qDim1QCjkbBI/96rc8b4MJg9Y60SCYnlgLZEEYPayzNV5hRw86N7sbdJ4z+vvKvGWgALuEjfbbuOrYA0dVUiN/kgeaRAsfHkjbh/offDAmhBJomyjYsagAGoHnoPwjaBxhvAScxgf5KhekToKoY8b03e4KuLURp8Z98HtpgB+cfLF2mGh/3Jr9g96rORMFv3IRESeY3xyeWl0odcyDVs9NonuMo4b4AoeKC4EJeA39WzINICz7RKRKwCPFSIreE32kKUcuA3J5XeMEP3N8f7jaEtsXBkP7wPpA==',
-          bucket: 'dianxinonline',
-          endpoint: 'https://oss-cn-beijing.aliyuncs.com',
+          region: '',
+          accessKeyId: '',
+          accessKeySecret: '',
+          stsToken: '',
+          bucket: '',
+          endpoint: '',
         });
       },
     },
